01e546c0589432b1f8ae02f7f07cf64a2072b9d9
velzie <velzie@velzie.rip>
2025-05-04 11:41:08 -0400

    more parsing

--- src/test.sh
+++ src/test.sh
@@ -4,6 +4,9 @@ export LC_ALL=C
 . src/util-pure.sh
 
 
+# rv32 instruction format
+# | 31–25 | 24–20 | 19–15 | 14–12 | 11–7  | 6–0   |
+# | funct7| rs2   | rs1   | funct3| rd    | opcode|
 parsei() {
     local b1 b2 b3 b4 hex
 
@@ -13,10 +16,18 @@ parsei() {
     b4=$(readn 1 | tohex)
     hex="$b4$b3$b2$b1"
     int=$((0x$hex))
-    op=$((int & 0xff))
-    echo $op
+
+    op=$((    int & 2#00000000000000000000000001111111))
+    rd=$((    int & 2#00000000000000000000111110000000))
+    funct3=$((int & 2#00000000000000000111000000000000))
+    rs1=$((   int & 2#00000000000011111000000000000000))
+    rs2=$((   int & 2#00000001111100000000000000000000))
+    funct7=$((int & 2#11111110000000000000000000000000))
+    echo "$op $rd $funct3 $rs1 $rs2 $funct7"
+
+
     case $op in
-        19)
+        51)
         rd=
         ;;
     esac
