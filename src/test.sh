dce2c8e1975aeb0d67afd1c8b9fda4f2bed9151e
velzie <velzie@velzie.rip>
2025-05-05 20:45:43 -0400

    rest of the instructions

--- src/test.sh
+++ src/test.sh
@@ -353,9 +353,11 @@ step() {
         echo "PC not aligned"
     fi
     int=$(memreadword $PC)
+    # printf "%08x" $int | sed 's/../& /g' | awk '{for(i=4;i>0;i--) printf $i}' | xxd -r -p > t
+    # riscv32-unknown-linux-gnu-objdump -D -b binary -m riscv:rv32 t | grep -P '^\s*[0-9a-f]+:\s+[0-9a-f]+\s+' | sed -E 's/^\s*[0-9a-f]+:\s+[0-9a-f]+\s+//'   # echo -en "$int: "
 
-    echo -en "$int: "
-    disasm $int
+    # printf "%08x" $int
+    # disasm $int
 
     local rval=0
     local rdid=$(((int >> 7) & 0x1f))
@@ -364,7 +366,7 @@ step() {
     rval=$((((((sum + INTMAX) % (INTMAX * 2)) + (INTMAX * 2)) % (INTMAX * 2)) - (INTMAX)))
 
     opcode=$((int & 0x7f))
-    echo "$PC"
+    # echo "$PC"
     case $opcode in
         $((0x37))) # LUI
             # U type
@@ -381,7 +383,8 @@ step() {
             # J type
             # (rd = pc + 4; pc += imm)
             imm=$(( ((int & 0x80000000) >> 11) | ( (int & 0x7fe00000) >> 20) | ((int & 0x00100000) >> 9) | (int & 0x000ff000) ))
-            sextendimm
+            # sext is -1,048,576 to +1,048,574
+            if ((imm & 0x00100000)); then imm=$((imm | 0xffffffffffe00000)); fi
             rval=$((PC + 4))
             PC=$((PC + imm - 4))
             ;;
@@ -406,13 +409,12 @@ step() {
             rdid=0
             funct3=$(((int >> 12) & 0x7))
             case $funct3 in
-                # BGE,
                 $((0x0))) if [ $rs1val -eq $rs2val ]; then PC=$jumpto; fi ;;
                 $((0x1))) if [ $rs1val -ne $rs2val ]; then PC=$jumpto; fi ;;
                 $((0x4))) if [ $rs1val -lt $rs2val ]; then PC=$jumpto; fi ;;
                 $((0x5))) if [ $rs1val -ge $rs2val ]; then PC=$jumpto; fi ;;
-                $((0x6))) if [ $rs1val -lt $rs2val ]; then PC=$jumpto; fi ;;
-                $((0x7))) if [ $rs1val -ge $rs2val ]; then PC=$jumpto; fi ;;
+                $((0x6))) if [ $rs1val -lt $rs2val ]; then PC=$jumpto; fi ;; # zero ext here
+                $((0x7))) if [ $rs1val -ge $rs2val ]; then PC=$jumpto; fi ;; # zero ext here
                 *) trap=3;;
             esac
             ;;
@@ -420,10 +422,11 @@ step() {
             rs1=$(((int >> 15) & 0x1f))
             rs1val=$((REGS[rs1]))
             imm=$((int >> 20))
-            sextendimm
+            # sign extend between -2048 and +2047
+            if (( imm & 0x800 )); then imm=$((imm | 0xfffffffffffff000)); fi
             rsval=$((rs1val + imm - RAM_IMAGE_OFFSET))
             if ((rsval > MEMSIZE - 4)); then
-                echo "poked out of bounds. uart?"
+                echo "peek out of bounds. uart?"
             else
                 funct3=$(((int >> 12) & 0x7))
                 case $funct3 in
@@ -450,10 +453,135 @@ step() {
                     *) trap=3;;
                 esac
             fi
+            ;;
+        $((0x23))) # STORE
+            rs1=$(((int >> 15) & 0x1f))
+            rs1val=$((REGS[rs1]))
+            rs2=$(((int >> 20) & 0x1f))
+            rs2val=$((REGS[rs2]))
+            imm=$(( ( ( int >> 7 ) & 0x1f ) | ( ( int & 0xfe000000 ) >> 20 ) ))
+            # sign extend between -2048 and +2047
+            if (( imm & 0x800 )); then imm=$((imm | 0xfffffffffffff000)); fi
+            rsval=$((rs1val + imm - RAM_IMAGE_OFFSET))
+            if ((rsval > MEMSIZE - 4)); then
+                echo "peek out of bounds. uart?"
+            else
+                funct3=$(((int >> 12) & 0x7))
+                case $funct3 in
+                    $((0x0))) # store byte
+                        memwritebyte $rsval $rs2val
+                    ;;
+                    $((0x1))) # store halfword
+                        memwritehalfword $rsval $rs2val
+                    ;;
+                    $((0x2))) # store word
+                        memwriteword $rsval $rs2val
+                    ;;
+                    *) trap=3;;
+                esac
+            fi
+            ;;
+        $((0x13))|$((0x33))) # OP/OP-IMM
+            imm=$((int >> 20))
+            if (( imm & 0x800 )); then imm=$((imm | 0xfffffffffffff000)); fi
+            rs1=$(((int >> 15) & 0x1f))
+            rs1val=$((REGS[rs1]))
+
+            # 0110011 for OP 0010011 for OP Immediate
+            # the 0x20 is the 0100000 difference
+            not_imm=$((int & 0x20))
+            if ((not_imm)); then
+                # both sets are the same just load from register if OP
+                rs2=$(((int >> 20) & 0x1f))
+                rs2val=$((REGS[rs2]))
+            else
+                # and immediate otherwise
+                rs2val=$imm
+            fi
+
+            funct3=$(((int >> 12) & 0x7))
+            if ((not_imm)) && ((int & 0x02000000)); then
+                echo "rv32M extension"
+            else
+                case $funct3 in
+                    0) # add/sub/addi
+                        # there's no subi because the imm is signed
+                        if ((not_imm)) && ((int & 0x40000000)); then # add and subtract differentiated by the funct7
+                            rval=$((rs1val - rs2val))
+                        else
+                            rval=$((rs1val + rs2val))
+                        fi
+                    ;;
+                    1) # sll/slli
+                        rval=$((rs1val << (rs2val & 0x1f)))
+                    ;;
+                    2) # slt/slti
+                        # this one compares the values and sets the result to 1 if true, 0 otherwise
+                        # thats why it looks weird
+                        if ((rs1val < rs2val)); then
+                            rval=1
+                        else
+                            rval=0
+                        fi
+                    ;;
+                    3)  # sltu/sltiu
+                        # same thing except unsigned/zero extended
+                        # todo
+                        if ((rs1val < rs2val)); then
+                            rval=1
+                        else
+                            rval=0
+                        fi
+                    ;;
+                    4)  # xor/xori
+                        rval=$((rs1val ^ rs2val))
+                    ;;
+                    5) # srl/sra/srli/srai
+                        # also differentiated by func7 (or imm but it's in the same place)
+                        if ((int & 0x40000000)); then
+                            # todo zero extend this one
+                            rval=$((rs1val >> (rs2val & 0x1f)))
+                        else
+                            rval=$((rs1val >> (rs2val & 0x1f)))
+                        fi
+                    ;;
+                    6) # or/ori
+                        rval=$((rs1val | rs2val))
+                    ;;
+                    7) # and/andi
+                        rval=$((rs1val & rs2val))
+                    ;;
+                esac
+            fi
+            ;;
+        $((0x0f)))
+            # dumbass fence thing
+            # this is a single core processor
+            rdid=0
+            ;;
+        $((0x73))) #Zifencei/Zicsr/ecall/ebreak
+            csrno=$((int >> 20))
+            funct3=$(((int >> 12) & 0x7))
+            if ((funct3 & 3)); then
+                echo "ziscr"
+            elif ((funct3 == 0)); then
+                echo "ecall"
+            else
+                trap=3
+            fi
 
             ;;
         *) echo "unknown opcode $opcode"
     esac
+
+    if ((trap)); then
+        echo "trap $trap"
+        exit 1
+    fi
+
+    if ((rdid)); then
+        REGS[rdid]=$rval
+    fi
 
     PC=$((PC + 4))
 }
