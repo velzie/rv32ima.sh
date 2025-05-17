0f2470c1b3c73f140e253318323570df13473579
FoxMoss <foxmoss@mediaology.com>
2025-05-17 12:46:58 -0500

    feat: puts impl

---
+++ lib/syscalls.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <stdio.h>
+
+static inline void write(const char *str, size_t len) {
+  asm volatile("li a7, 64;"
+               "mv a1, %[str];"
+               "mv a2, %[len];"
+               "ecall;"
+               :
+               : [str] "r"(str), [len] "r"(len)
+               : "a7", "a1", "a2");
+}
--- main.c
+++ main.c
@@ -1,18 +1,2 @@
-void _start() {
-  const char str[] = "Hello World!\n\0";
-  const char str2[] = "World 2!\n\0";
-  asm volatile("li a7, 64;"
-               "mv a1, %[str];"
-               "li a2, 14;"
-               "ecall;"
-               :
-               : [str] "r"(&str)
-               : "a7", "a1", "a2");
-  asm volatile("li a7, 64;"
-               "mv a1, %[str2];"
-               "li a2, 9;"
-               "ecall;"
-               :
-               : [str2] "r"(&str2)
-               : "a7", "a1", "a2");
-}
+#include <stdio.h>
+void _start() { printf("Hello from printf \n"); }
